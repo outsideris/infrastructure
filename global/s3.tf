d65ad242a294baaadc8255ecd97390b63b53a6ad
Outsider <outsideris@gmail.com>
2017-04-18 22:26:15 +0900

    add s3 bucket to keep terraform state

--- global/s3.tf
+++ global/s3.tf
@@ -12,3 +12,29 @@ resource "aws_s3_bucket" "vault-test-outsider" {
   bucket = "vault-test-outsider"
   acl    = "private"
 }
+
+// sideeffect 용 terraform state
+resource "aws_s3_bucket" "terraform-state" {
+  bucket = "kr.sideeffect.terraform.state"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  tags {
+    Name        = "terraform state"
+    Environment = "Prod"
+  }
+  logging {
+    target_bucket = "${aws_s3_bucket.logs.id}"
+    target_prefix = "log/"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+// 로깅용 버킷
+resource "aws_s3_bucket" "logs" {
+  bucket = "kr.sideeffect.logs"
+  acl    = "log-delivery-write"
+}
