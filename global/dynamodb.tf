42d1003263193cd5838bcf022a028e5bcece7723
Outsider <outsideris@gmail.com>
2017-04-19 23:06:23 +0900

    add dynamodb table for locking terraform state

---
+++ global/dynamodb.tf
@@ -0,0 +1,12 @@
+// terrafrom state 파일을 lock 테이블
+resource "aws_dynamodb_table" "terraform-lock" {
+  name = "SideEffectTerraformStateLock"
+  read_capacity = 5
+  write_capacity = 5
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
