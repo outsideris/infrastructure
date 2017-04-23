0f051ae31dff185673993791b43fdc176ea8f8c6
Outsider <outsideris@gmail.com>
2017-04-23 22:33:06 +0900

    remove all vpc and keep default vpc

--- global/vpc.tf
+++ global/vpc.tf
@@ -5,17 +5,6 @@ resource "aws_vpc" "side-effect" {
   instance_tenancy = "default"
 
   tags {
-    "Name" = "side effect"
-  }
-}
-
-resource "aws_vpc" "test-field" {
-  cidr_block = "172.10.0.0/20"
-  enable_dns_hostnames = true
-  enable_dns_support = true
-  instance_tenancy = "default"
-
-  tags {
-    "Name" = "test field"
+    "Name" = "default"
   }
 }
