bab3e3b55b58a84cb5903d2d47eaeed717f9618a
Outsider <outsideris@gmail.com>
2017-05-10 21:51:30 +0900

    import default vpc

---
+++ vpc/default-vpc.tf
@@ -0,0 +1,73 @@
+// default VPC
+// do not destroy and keep it do nothing
+resource "aws_vpc" "default" {
+  cidr_block  = "172.31.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  instance_tenancy = "default"
+  tags {
+    "Name" = "default"
+  }
+}
+
+resource "aws_route_table" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+  tags {
+    "Name" = "default"
+  }
+}
+
+resource "aws_main_route_table_association" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+  route_table_id = "${aws_route_table.default.id}"
+}
+
+resource "aws_network_acl" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+  subnet_ids = []
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    rule_no = 100
+    action = "allow"
+    protocol = "-1"
+    cidr_block = "0.0.0.0/0"
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    rule_no = 100
+    action = "allow"
+    protocol = "-1"
+    cidr_block = "0.0.0.0/0"
+  }
+
+  tags {
+    "Name" = "default"
+  }
+}
+
+resource "aws_security_group" "default" {
+  name = "default"
+  description = "default VPC security group"
+  vpc_id = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+  "Name" = "default"
+  }
+}
