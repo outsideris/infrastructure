9bbbbc0e271e18900ed32c542da93002125f7d2f
Outsider <outsideris@gmail.com>
2017-06-17 20:58:05 +0900

    open outbound network from private subnet

--- vpc/sideeffect-vpc.tf
+++ vpc/sideeffect-vpc.tf
@@ -261,25 +261,6 @@ resource "aws_network_acl_rule" "side_effect_public_egress_ephemeral" {
 // network acl for private subnets
 resource "aws_network_acl" "side_effect_private" {
   vpc_id = "${aws_vpc.side_effect.id}"
-
-  egress {
-    protocol = -1
-    rule_no = 100
-    action = "allow"
-    cidr_block = "${aws_vpc.side_effect.cidr_block}"
-    from_port = 0
-    to_port = 0
-  }
-
-  ingress {
-    protocol = -1
-    rule_no = 100
-    action = "allow"
-    cidr_block = "${aws_vpc.side_effect.cidr_block}"
-    from_port = 0
-    to_port = 0
-  }
-
   subnet_ids = [
     "${aws_subnet.side_effect_private_subnet1.id}",
     "${aws_subnet.side_effect_private_subnet2.id}"
@@ -288,6 +269,61 @@ resource "aws_network_acl" "side_effect_private" {
   tags {
     Name = "private"
   }
+}
+
+resource "aws_network_acl_rule" "side_effect_private_ingress_vpc" {
+  network_acl_id = "${aws_network_acl.side_effect_private.id}"
+  rule_number = 100
+  rule_action = "allow"
+  egress = false
+  protocol = -1
+  cidr_block = "${aws_vpc.side_effect.cidr_block}"
+  from_port = 0
+  to_port = 0
+}
+
+resource "aws_network_acl_rule" "side_effect_private_egress_vpc" {
+  network_acl_id = "${aws_network_acl.side_effect_private.id}"
+  rule_number = 100
+  rule_action = "allow"
+  egress = true
+  protocol = -1
+  cidr_block = "${aws_vpc.side_effect.cidr_block}"
+  from_port = 0
+  to_port = 0
+}
+
+resource "aws_network_acl_rule" "side_effect_private_ingress_nat" {
+  network_acl_id = "${aws_network_acl.side_effect_private.id}"
+  rule_number = 110
+  rule_action = "allow"
+  egress = false
+  protocol = "tcp"
+  cidr_block = "0.0.0.0/0"
+  from_port = 1024
+  to_port = 65535
+}
+
+resource "aws_network_acl_rule" "side_effect_private_egress80" {
+  network_acl_id = "${aws_network_acl.side_effect_private.id}"
+  rule_number = 120
+  rule_action = "allow"
+  egress = true
+  protocol = "tcp"
+  cidr_block = "0.0.0.0/0"
+  from_port = 80
+  to_port = 80
+}
+
+resource "aws_network_acl_rule" "side_effect_private_egress443" {
+  network_acl_id = "${aws_network_acl.side_effect_private.id}"
+  rule_number = 130
+  rule_action = "allow"
+  egress = true
+  protocol = "tcp"
+  cidr_block = "0.0.0.0/0"
+  from_port = 443
+  to_port = 443
 }
 
 // Basiton Host
