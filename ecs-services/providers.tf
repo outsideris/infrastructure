369ca0db3f91e0989b8678493cc292dbb64bf2b9
Outsider <outsideris@gmail.com>
2017-09-19 18:10:45 -0500

    specify providers's version rules

---
+++ ecs-services/providers.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  version = "~> 0.1.4"
+  region     = "ap-northeast-1"
+}
+
+provider "terraform" {
+  version = "~> 1.0.0"
+}
--- us-east-1/providers.tf
+++ us-east-1/providers.tf
@@ -1,3 +1,4 @@
 provider "aws" {
+  version = "~> 0.1.4"
   region = "us-east-1"
 }
