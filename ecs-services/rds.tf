4cfb1552a01e86efd2d5dd92b99c23d251c53548
Outsider <outsideris@gmail.com>
2017-10-31 15:45:59 +0900

    create rds instance for stress test with node v8

--- ecs-services/rds.tf
+++ ecs-services/rds.tf
@@ -28,3 +28,34 @@ module "pinkfong6_db" {
   family = "postgres9.6"
   final_snapshot_identifier = "pinkfong-6-db"
 }
+
+module "pinkfong8_db" {
+  source = "terraform-aws-modules/rds/aws"
+
+  identifier = "pinkfong8"
+
+  engine            = "postgres"
+  engine_version    = "9.6.3"
+  instance_class    = "db.m4.large"
+  allocated_storage = 10
+  storage_encrypted = false
+
+  name = "pinkfong8"
+
+  username = "outsider"
+
+  password = ""
+  port     = "5432"
+
+  vpc_security_group_ids = ["${data.terraform_remote_state.vpc.side_effect_default_sg}"]
+
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  backup_retention_period = 0
+
+  subnet_ids = ["${data.terraform_remote_state.vpc.side_effect_private_subnets}"]
+
+  family = "postgres9.6"
+  final_snapshot_identifier = "pinkfong-8-db"
+}
