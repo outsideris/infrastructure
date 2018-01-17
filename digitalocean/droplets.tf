722cc4f93da1f9244e811f947d985c6f7fc57c2c
Outsider <outsideris@gmail.com>
2018-01-17 23:49:44 +0900

    use flexible droplets
    It is new plan.

--- digitalocean/droplets.tf
+++ digitalocean/droplets.tf
@@ -2,7 +2,7 @@ resource "digitalocean_droplet" "blog" {
   image      = "ubuntu-16-04-x64"
   name       = "blog"
   region     = "sgp1"
-  size       = "2gb"
+  size       = "s-2vcpu-2gb"
   monitoring = true
   ipv6       = true
   ssh_keys   = ["5c:62:af:d8:28:72:d7:36:eb:29:47:ec:7d:90:8d:28"]
