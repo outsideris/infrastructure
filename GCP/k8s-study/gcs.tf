634fa25442f4786b0a310e2fb09661a393cae447
Outsider <outsideris@gmail.com>
2018-05-22 17:02:40 +0900

    gcp: set to delete storage for k8s-study
    Signed-off-by: Outsider <outsideris@gmail.com>

--- GCP/k8s-study/gcs.tf
+++ GCP/k8s-study/gcs.tf
@@ -1,6 +1,7 @@
 resource "google_storage_bucket" "terraform_state" {
   name     = "kr-sideeffect-terraform-state"
   location = "asia-northeast1"
+  force_destroy = "true"
 
   versioning = {
     enabled = "true"
