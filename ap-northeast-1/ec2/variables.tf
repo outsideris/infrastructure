6cc9fb2f623f4b29c6d6fbeae3715259349f1589
Outsider <outsideris@gmail.com>
2019-12-25 19:29:22 +0900

    use dynamodb_table instead deprecated lock_table
    Signed-off-by: Outsider <outsideris@gmail.com>

--- ap-northeast-1/ec2/variables.tf
+++ ap-northeast-1/ec2/variables.tf
@@ -7,12 +7,12 @@ data "terraform_remote_state" "global" {
   backend = "s3"
 
   config = {
-    bucket     = "kr.sideeffect.terraform.state"
-    key        = "global/terraform.tfstate"
-    region     = "ap-northeast-1"
-    encrypt    = true
-    lock_table = "SideEffectTerraformStateLock"
-    acl        = "bucket-owner-full-control"
+    bucket         = "kr.sideeffect.terraform.state"
+    key            = "global/terraform.tfstate"
+    region         = "ap-northeast-1"
+    encrypt        = true
+    dynamodb_table = "SideEffectTerraformStateLock"
+    acl            = "bucket-owner-full-control"
   }
 }
 
@@ -21,12 +21,12 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
 
   config = {
-    bucket     = "kr.sideeffect.terraform.state"
-    key        = "ap-northeast-1/vpc/terraform.tfstate"
-    region     = "ap-northeast-1"
-    encrypt    = true
-    lock_table = "SideEffectTerraformStateLock"
-    acl        = "bucket-owner-full-control"
+    bucket         = "kr.sideeffect.terraform.state"
+    key            = "ap-northeast-1/vpc/terraform.tfstate"
+    region         = "ap-northeast-1"
+    encrypt        = true
+    dynamodb_table = "SideEffectTerraformStateLock"
+    acl            = "bucket-owner-full-control"
   }
 }
 
