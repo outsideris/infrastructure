9bb4ee76c2799d556c99abdd7528b4f0f44edebb
Outsider <outsideris@gmail.com>
2021-08-23 21:40:41 +0900

    remove unused resources
    Signed-off-by: Outsider <outsideris@gmail.com>

--- ap-northeast-1/apprunner/ecr.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "popularconvention" {
-  name                 = "popularconvention"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-}
--- ap-northeast-1/apprunner/providers.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  region = "ap-northeast-1"
-}
-
