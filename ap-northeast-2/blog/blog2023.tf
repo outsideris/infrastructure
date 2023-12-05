83be483709f96b4dbda4c234772b194bab58eb70
Outsider <outsideris@gmail.com>
2023-12-06 02:46:57 +0900

    create new instance for blog
    Signed-off-by: Outsider <outsideris@gmail.com>

---
+++ ap-northeast-2/blog/blog2023.tf
@@ -0,0 +1,30 @@
+resource "aws_lightsail_instance" "blog_2023" {
+  name              = "blog.outsider.ne.kr"
+  availability_zone = data.aws_availability_zones.available.names[0]
+  blueprint_id      = "ubuntu_22_04"
+  bundle_id         = "medium_3_0"
+  key_pair_name     = "id_rsa"
+
+  connection {
+    user        = "ubuntu"
+    type        = "ssh"
+    private_key = file(var.lightsail_private_key)
+    host        = self.public_ip_address
+    timeout     = "1m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y software-properties-common",
+    ]
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      echo "[blog]\n${self.public_ip_address} ansible_connection=ssh ansible_ssh_user=ubuntu" > inventory
+      # ansible-playbook -i inventory init.yml
+      # ansible-playbook -i inventory letsencrypt.yml
+EOF
+  }
+}
