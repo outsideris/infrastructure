0cd68975b25c49a7532d3b0fee1c7f027aac6f45
Outsider <outsideris@gmail.com>
2024-02-03 12:55:06 +0900

    update blog instance
    Signed-off-by: Outsider <outsideris@gmail.com>

--- cloudflare/zone-outsider-dev.tf
+++ cloudflare/zone-outsider-dev.tf
@@ -2,3 +2,20 @@ resource "cloudflare_zone" "outsider_dev" {
   account_id = var.cloudflare_account_id
   zone       = "outsider.dev"
 }
+
+resource "cloudflare_record" "teslamate" {
+  zone_id = cloudflare_zone.outsider_dev.id
+  name    = "teslamate"
+  value   = "158.247.242.91"
+  type    = "A"
+  ttl     = 3600
+  proxied = false
+}
+resource "cloudflare_record" "tesla_grafana" {
+  zone_id = cloudflare_zone.outsider_dev.id
+  name    = "tesla-grafana"
+  value   = "158.247.242.91"
+  type    = "A"
+  ttl     = 3600
+  proxied = false
+}
